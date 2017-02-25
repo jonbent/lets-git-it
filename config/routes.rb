1d9f41ae4afbcc3cba11a85ecdbb2df91461eb63
George Babayan <ggbabayan@gmail.com>
2017-02-24 16:27:01 -0800

    Add line 3 in routes for challenges

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    get 'homes/index'
-    root 'homes#index'
+  resources :challenges, except:[:destroy]
+  
+  get 'homes/index'
+  root 'homes#index'
 
-    resources :users
+  resources :users
 
-    get 'auth/developer', as: 'developer_auth'
-    get 'auth/github', as: 'github_auth'
+  get 'auth/developer', as: 'developer_auth'
+  get 'auth/github', as: 'github_auth'
 
-    match 'auth/:provider/callback' => 'session#create', :via => [:get, :post]
+  match 'auth/:provider/callback' => 'session#create', :via => [:get, :post]
 
-    get 'session/destroy', as: 'logout'
+  get 'session/destroy', as: 'logout'
 
 end
