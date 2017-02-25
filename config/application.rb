056f67fa60c17410052d7861475b48e23d107f17
George Babayan <ggbabayan@gmail.com>
2017-02-24 16:07:39 -0800

    Adds login and logout with OmniAuth

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,8 @@ module LetsGitIt
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.middleware.use OmniAuth::Builder do
+      provider :developer
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get 'homes/index'
+    root 'homes/index'
+
+    resources :users
+
+    get 'auth/developer', :as => 'developer_auth'
+
+    match 'auth/:provider/callback' => 'session#create', :via => [:get, :post]
+
+    get 'session/destroy', as: 'logout'
+
 end
