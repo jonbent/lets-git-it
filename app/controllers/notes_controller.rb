a4d91573b6b2bed543b6fb7e304cb96ef9f8a070
George Babayan <ggbabayan@gmail.com>
2017-02-26 11:15:18 -0800

    makes notes nested in scoreboard, adds broken forms

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,9 +4,13 @@ class NotesController < ApplicationController
 	@day = "week1wed" #we kind of need it?
 
 	def index
+		@scoreboard = Scoreboard.find(params[:scoreboard_id])
 		@notes = Note.all.order(:note_type)
 	end
 
+	def new
+		@note = Note.new
+	end
 	def create
 		@note = Note.new(params[:note])
 	end
