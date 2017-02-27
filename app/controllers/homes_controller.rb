6810c9c717cc476b896d41c8672333bafe84f088
George Babayan & Jon Bent <pair+georgebabayan.jonbent@devbootcamp.com>
2017-02-27 11:11:47 -0800

    Add more secure staff

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,23 +4,23 @@ class HomesController < ApplicationController
   def index
     # @current_user ||= User.find_by_id(session[:user])
 
-    client = Octokit::Client.new(:access_token => "f376fdd4f3102ca52939c69295be83d5994f8c17")
+    # client = Octokit::Client.new(:access_token => "")
+    #
+    # # client = Octokit::Client.new(:login => 'georgebabayan', :password => 'snm4k7d')
+    #
+    #
+    # user = Octokit.user client.login
+    #
+    # # Get the repos rel, returned from the API
+    # # as repos_url in the resource
+    # user.rels[:repos].href
+    # # => "https://api.github.com/users/technoweenie/repos"
+    #
+    # repos = user.rels[:repos].get.data
+    # repos.last.name
+    # # => "faraday-zeromq"
 
-    # client = Octokit::Client.new(:login => 'georgebabayan', :password => 'snm4k7d')
 
-
-    user = Octokit.user client.login
-
-    # Get the repos rel, returned from the API
-    # as repos_url in the resource
-    user.rels[:repos].href
-    # => "https://api.github.com/users/technoweenie/repos"
-
-    repos = user.rels[:repos].get.data
-    repos.last.name
-    # => "faraday-zeromq"
-
-
-    @github_response = repos
+    # @github_response = repos
   end
 end
