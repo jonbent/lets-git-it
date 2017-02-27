de9c43a4a3c79413a6dc3b2afaef4fc8b32eef68
georgecode <georgeczurlanis@gmail.com>
2017-02-26 18:34:15 -0800

    Move header to layout,add dummy content,add call back and delays to slider left slider done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,7 +18,7 @@
 
 $(document).ready(function(){
 
-var homeSlide = document.getElementById('body');
+var homeSlide = document.getElementById('home');
 var slider = new Hammer(homeSlide)
 slider.on("panleft panright tap press", function(ev) {
       // homeSlide.textContent = ev.type +" gesture detected.";
@@ -31,9 +31,51 @@ slider.on("panleft panright tap press", function(ev) {
         console.log("panright")
         // $('#body').css("background-color","pink")
         $('#leftPage').addClass("leftPage");
+        $('#home').addClass("afterSwipeLeftHome");
+      }
+  });//End Home slider
+
+
+
+var leftPageSlide = document.getElementById('leftPage');
+var slider = new Hammer(leftPageSlide)
+slider.on("panleft panright tap press", function(ev) {
+      // homeSlide.textContent = ev.type +" gesture detected.";
+      // console.log(ev.type);
+      if(ev.type=="panleft"){
+      // console.log("panleft")
+      // $('#body').css("background-color","green")
+       $('#leftPage').removeClass("leftPage", callBack());
+       // document.getElementById("#leftPage").addEventListener("transitionend", callBack());
+          // $('#home').removeClass("afterSwipeLeftHome");
+          // $('#home').show();
+
+       // });
+
+
+
+
+      }else if(ev.type=="panright"){
+        // console.log("panright")
+        // $('#body').css("background-color","pink")
+        $('#leftPage').addClass("leftPage");
       }
   });
 
-
 });//END DOCUMENT READY
 
+
+
+function callBack() {
+  console.log("FUCCCCCKKKKK");
+
+  setTimeout(function(){
+    $('#home').removeClass("afterSwipeLeftHome");
+  }, 400);
+
+   // $('#home').removeClass("afterSwipeLeftHome");
+
+
+    // this.innerHTML = "transitionend event occured - The transition has completed";
+    // this.style.backgroundColor = "pink";
+}
