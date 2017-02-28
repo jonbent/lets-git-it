3bb2974256aca49b2f06e4a87ffc463f9741dde0
slinkous <sralinkous@gmail.com>
2017-02-27 17:57:16 -0800

    Snapshot for pull

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'homes/index'
-  root 'scoreboards#new'
+  root 'homes#index'
 
   resources :users, only: [:index, :edit, :destroy, :create]
 
