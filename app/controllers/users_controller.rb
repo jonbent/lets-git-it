99463ebaebeebd69e2c6d09a5896dd79d2f85381
George Babayan <ggbabayan@gmail.com>
2017-02-28 21:51:41 -0800

    Add statistic for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 	  @user_scoreboards = @user.scoreboards
+		@total_days_points = @user_scoreboards.map(&:day_points).reduce(:+)
+		@total_commits = @user_scoreboards.map(&:commits).reduce(:+)
 	end
 
 end
