94597cf4509c6612f947ceb8c5b6b13a5c267f9d
slinkous <sralinkous@gmail.com>
2017-02-28 18:01:44 -0800

    Display scores for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,19 +10,7 @@ class User < ApplicationRecord
 	end
 
 	def level
-		score = self.total_points_change
-		# unless score
-		# 	score = 0
-		# end
-		# case score
-		# when score < 10
-		# 	return 1
-		# when 10 < score < 30
-		# 	return 2
-		# when 30 < score < 100
-		# 	return 3
-		# end
-		1
+		level = Math.sqrt(self.total_points).floor
 	end
 
 end
