1fea04821e31970b2ccf73b9b5d14920aca82c23
George Babayan <ggbabayan@gmail.com>
2017-02-28 21:19:18 -0800

    Add leaderboard and user_stats

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,9 @@ class UsersController < ApplicationController
 		@scoreboard = current_scoreboard
 	end
 
+	def show
+		@user = User.find(params[:id])
+	  @user_scoreboards = @user.scoreboards
+	end
+
 end
