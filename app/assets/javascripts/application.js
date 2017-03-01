d001aefeeb3db50dbef88d685efcf06fa08bf863
George Babayan <ggbabayan@gmail.com>
2017-03-01 11:27:02 -0800

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,9 +16,6 @@
 //= require turbolinks
 //= require Chart.bundle
 //= require chartkick
-//= require fusioncharts/fusioncharts
-//= require fusioncharts/fusioncharts.charts
-//= require fusioncharts/themes/fusioncharts.theme.fint
 
 $(document).ready(function(){
 
