53e07b1bb81da151e9328bdfc9c840f2c3b3a1df
Jon <jon.bent@me.com>
2017-03-02 11:15:56 -0800

    fixes slider

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,45 +19,30 @@
 
 $(document).ready(function(){
 
-  var homeSlide = document.getElementById('home');
-  var slider = new Hammer(homeSlide)
-  slider.on("panleft panright tap press", function(ev) {
-        console.log(ev.type);
-        if(ev.type=="panleft"){
-          // console.log("panleft")
-         $('#rightPage').addClass("rightPage");
-         $('#home').addClass("afterSwipeRightHome");
-        }else if(ev.type=="panright"){
-          // console.log("panright")
-          $('#leftPage').addClass("leftPage");
-          $('#home').addClass("afterSwipeLeftHome");
-        }
-    });//End Home slider
+
+  new Hammer( $( ".fixSafariOverflow" )[ 0 ], {
+        domEvents: true
+      } );
+      var current = 1;
+      var pgs = $( ".home-page-slider" );
+      $( ".fixSafariOverflow" ).on( "swipeleft", function( e ) {
+       if ( pgs[ current + 1 ] ) {
+         pgs.removeClass( "home" );
+         pgs.addClass('hidden')
+         pgs.eq( ++current ).addClass( "home" );
+         pgs.eq(current).removeClass( "hidden" );
+       }
+      } );
+      $( ".fixSafariOverflow" ).on( "swiperight", function( e ) {
+       if ( pgs[ current - 1 ] ) {
+         pgs.removeClass( "home" );
+         pgs.addClass('hidden')
+         pgs.eq( --current ).addClass( "home" );
+         pgs.eq(current).removeClass( "hidden" );
+       }
+      } );
 
 
-  var leftPageSlide = document.getElementById('leftPage');
-  var slider = new Hammer(leftPageSlide)
-  slider.on("panleft panright tap press", function(ev) {
-    if(ev.type=="panleft"){
-      // console.log("panleft")
-     $('#leftPage').removeClass("leftPage", homeAfterLeft());
-    }else if(ev.type=="panright"){
-      // console.log("panright")
-      $('#leftPage').addClass("leftPage");
-    }
-  });//End left Page slider
-
-
-  var rightPageSlide = document.getElementById('rightPage');
-  var slider = new Hammer(rightPageSlide)
-  slider.on("panleft panright tap press", function(ev) {
-     if(ev.type=="panleft"){
-       // console.log("panleft")
-     }else if(ev.type=="panright"){
-       // console.log("panright")
-       $('#rightPage').removeClass("rightPage", homeAfterRight());
-     }
-  });//End RightPage slider
 
   addNoteToDatabase()
   addEngineeringEmpathyPoint();
@@ -68,14 +53,14 @@ $(document).ready(function(){
 function homeAfterLeft() {
   setTimeout(function(){
     $('#home').removeClass("afterSwipeLeftHome");
-  }, 400);
+  }, 75);
 }
 
 
 function homeAfterRight(){
     setTimeout(function(){
     $('#home').removeClass("afterSwipeRightHome");
-    }, 400);
+  }, 75);
 }
 
 var addNoteToDatabase = function(){
