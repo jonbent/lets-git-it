c8504c6b7df5a9b99e4383ff7d22cd5dd4771b26
Jon <jon.bent@me.com>
2017-03-17 15:53:12 -0700

    fixes background color and image stuff

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,10 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-# Dotenv.load
+
+Dotenv::Railtie.load
+
+HOSTNAME = ENV['HOSTNAME']
 module LetsGitIt
   class Application < Rails::Application
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
